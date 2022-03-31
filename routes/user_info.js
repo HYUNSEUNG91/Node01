0c5fa8839c914ab4af69bd1cccb40b91ecdabcfc
HYUNSEUNG91 <lhs91001@naver.com>
2022-03-31 13:51:13 +0900

    유효성 검사 추가

--- routes/user_info.js
+++ routes/user_info.js
@@ -14,26 +14,45 @@ const decoded = jwt.decode("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZXN0Ijp0cnV
     var result={result:true , msg:""};
     // 공백 or 빈값 check --> undefined, null, ""
 
+    //한글,영문,숫자 3~15자리 가능.
+    var nicknameReg = /^[a-z]+[a-z0-9]{2,15}$/g;
+    var emailReg = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
+
+    
+
     // nickname 길이 check
     if(data.nickname == undefined || data.nickname == null ||data.nickname == ""){
         result.msg ="닉네임을 입력해주세요."; // 회원가입 API --> res.send로 전달
         result.result=false;
         return result;
-    }else if(data.nickname.length<4){
-        result.msg ="닉네임 3이상";
+    }else if(!nicknameReg.test(data.nickname)){
+        result.msg ="닉네임은 3자이상 및 알파벳과 숫자만 사용 가능합니다.";
         result.result=false;
         return result;
-    // }else if(data.nickname == 특수문자x){
 
     }else if(data.email==undefined || data.email==null ||data.email==""){
         result.msg ="이메일을 입력해주세요.";
         result.result=false;
         return result;
+
+    }else if(!emailReg.test(data.email)){
+        result.msg ="이메일을 형식이 올바르지 않습니다.";
+        result.result=false;
+        return result;   
+
+    }else if(data.password.length < 5 && data.confirmPassword < 5){
+        result.msg ="비밀번호는 최소 4자 이상이어야 합니다.";
+        result.result=false;
+        return result; 
+
+    }else if(!data.password.search(data.nickname)){
+        result.msg ="비밀번호는 닉네임과 같은 값이 포함될 수 없습니다.";
+        result.result=false;
+        return result; 
     }else if(data.password !== data.confirmPassword){
         result.msg ="비밀번호를 확인하세요";
         result.result=false;
-        return result;
-        
+        return result; 
     }
     return result;    
 }
@@ -63,7 +82,7 @@ router.post("/users", async (req, res) => {
         })
         return;
     }
-    ///
+    //중복확인 유효성 검사
     const existUse = await User.find({
         $or : [{nickname}, {email}], //or 조건 표시 --> $or
     });
